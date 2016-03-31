7cbe6977153330e572b767e4022c63f9caadaa97
Constantine Melnikov <ka.melnikov@gmail.com>
2016-03-31 13:38:26 +0800

    Use Promise

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 var express = require('express');
 var app = express();
 var mongo = require('mongodb');
-var assert = require('assert');
-var async = require('async');
 var bodyParser = require('body-parser');
 
-var dbConn;
-
-mongo.MongoClient.connect('mongodb://localhost:27017/anychart_sample', function(err, db) {
-    assert.equal(null, err);
-    console.log("Connected correctly to mongodb server.");
-    dbConn = db;
-});
+const connection = mongo.MongoClient
+    .connect('mongodb://localhost:27017/anychart_sample')
+    .then(function(db) {
+        console.log("Connected correctly to mongodb server.");
+        return db;
+    })
+    .catch(function(err) {
+        console.error(err);
+        process.exit(1);
+    });
 
 app.use(express.static('public'));
 app.use(bodyParser.json({type: '*/json'}));
@@ -20,26 +21,30 @@ app.get('/', function (req, res) {
     res.sendfile(__dirname+'/public/index.html');
 });
 
-var asyncFind = function(collection) {
-    return function(callback) {
-        dbConn.collection(collection).find().toArray(callback);
-    };
-};
-
 app.get('/init', function(req, res) {
-    async.parallel([
-        asyncFind('regions'), asyncFind('industries'), asyncFind('products'), asyncFind('sales_reps'),
-        function(callback) {
-            dbConn.collection('sales').aggregate([{ $group: { "_id": {$year: "$date"}}}]).toArray(callback);
-        }
-    ], function(err, results) {       
-        res.json({
-            'regions': results[0],
-            'industries': results[1],
-            'products': results[2],
-            'sales_reps': results[3]
+    connection.then(function(db){
+        Promise.all([
+            db.collection('regions').find().toArray(),
+            db.collection('industries').find().toArray(),
+            db.collection('products').find().toArray(),
+            db.collection('sales_reps').find().toArray(),
+            db.collection('sales').aggregate([{ $group: { "_id": {$year: "$date"}}}]).toArray()
+        ])
+        .then(function(results){
+            res.json({
+                'regions': results[0],
+                'industries': results[1],
+                'products': results[2],
+                'sales_reps': results[3]
+            });
+        })
+        .catch(function(err){
+            console.error(err); res.send(err)
         });
-    });
+    })
+    .catch(function(err){
+        console.log(err)
+    })
 });
 
 app.post('/data', function(req, res) {
@@ -47,33 +52,39 @@ app.post('/data', function(req, res) {
                     {"product": {"$in": req.body.products.map(mongo.ObjectId)},
                      "region": {"$in": req.body.regions.map(mongo.ObjectId)},
                      "sales_rep": {"$in": req.body.sales.map(mongo.ObjectId)}}};
-    async.parallel([
-        function(callback) {
-            dbConn.collection('sales').aggregate([
+
+    connection.then(function(db){
+        Promise.all([
+            db.collection('sales').aggregate([
                 filters,
                 { $group: { _id: "$product", revenue: {$sum: "$total"} }}
-            ]).toArray(callback);
-        },
-        function(callback) {
-            dbConn.collection('sales').aggregate([
+            ]).toArray(),
+            db.collection('sales').aggregate([
                 filters,
                 { $group: { _id: "$sales_rep", revenue: {$sum: "$total"} }}
-            ]).toArray(callback);
-        },
-        function(callback) {
-            dbConn.collection('sales').aggregate([
+            ]).toArray(),
+            db.collection('sales').aggregate([
                 filters,
                 { $group: { _id: { quarter: {$cond: [{$lte:[{$month:"$date"},3]}, 1,
-                                                     {$cond: [{$lte:[{$month:"$date"},6]}, 2,
-                                                              {$cond: [{$lte:[{$month:"$date"},9]},
-                                                                       3, 4]}]}]},
-                                   year: {$year: "$date"}},
-                            revenue: {$sum: "$total"} }}]).toArray(callback);
-        }
-    ], function(err, results) {
-        res.json({'by_product': results[0],
-                  'by_sales': results[1],
-                  'by_quarter': results[2]});
+                    {$cond: [{$lte:[{$month:"$date"},6]}, 2,
+                        {$cond: [{$lte:[{$month:"$date"},9]},
+                            3, 4]}]}]},
+                    year: {$year: "$date"}},
+                    revenue: {$sum: "$total"} }}]).toArray()
+        ])
+        .then(function(results){
+            res.json({
+                'by_product': results[0],
+                'by_sales': results[1],
+                'by_quarter': results[2]
+            });
+        })
+        .catch(function(err){
+            console.error(err); res.send(err)
+        });
+    })
+    .catch(function(err){
+        console.log(err)
     });
 });
 
